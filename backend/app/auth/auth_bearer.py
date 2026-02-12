40b5facdf0a8bf647e36ca02b2d811d8fd51e42e
Nagalahari13 <kowtharapunagalahari@gmail.com>
2026-02-12 20:43:36 +0530

    Update auth_bearer.py

--- backend/app/auth/auth_bearer.py
+++ backend/app/auth/auth_bearer.py
@@ -11,7 +11,7 @@ class JWTBearer(HTTPBearer):
             raise HTTPException(status_code=403, detail="Invalid token scheme")
         return credentials.credentials
 
-# ✔✔ THIS IS THE REAL FIX (You were missing this)
+
 def get_current_user(token: str = Depends(JWTBearer())):
     user = decodeJWT(token)
     if not user:
